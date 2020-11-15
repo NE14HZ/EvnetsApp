b7c71a9d45bda98727be53e539ab993bd39be2af
Gaoquan <ligaoquanhhh@163.com>
2020-11-15 16:27:08 +0800

    New project of Spring Boot framwork

---
+++ RearEnd/eventspro/src/main/java/com/terri/hd/eventspro/EventsproApplication.java
@@ -0,0 +1,13 @@
+package com.terri.hd.eventspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsproApplication.class, args);
+	}
+
+}
---
+++ RearEnd/eventspro/src/test/java/com/terri/hd/eventspro/EventsproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terri.hd.eventspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
