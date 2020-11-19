4ed8c52025c97809411db1fd52178ab1b9fb652d
Gaoquan <ligaoquanhhh@163.com>
2020-11-19 23:41:25 +0800

    修复图片上传bug

--- RearEnd/eventspro/src/main/java/com/terri/hd/eventspro/config/OssConfig.java
+++ RearEnd/eventspro/src/main/java/com/terri/hd/eventspro/config/OssConfig.java
@@ -2,8 +2,9 @@ package com.terri.hd.eventspro.config;
 
 public class OssConfig {
     public static String endpoint = "http://oss-cn-chengdu.aliyuncs.com";
-    public static String accessKeyId = "LTAI4GHwXMRCUn1FYsoK5E3C";
-    public static String accessKeySecret = "kz1Dz1Sfgus3J3MRoFgD48ZzlbTYev";
+    public static String accessKeyId = "LTAI4GH1JGpgezz1qTbcyh62";
+    public static String accessKeySecret = "OgVYygKw0YurzHei5zM39kG9tCKEFc";
     public static String bucketName = "event-lee";
-    public static String fileHost = "event-lee/pics/";
+    public static String fileHost = "event-lee/pics";
+    public static String urlPrefix = "http://event-lee.oss-cn-chengdu.aliyuncs.com";
 }
